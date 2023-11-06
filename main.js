885f19d1551d9752ef4915d07b3efaa3106de185
Bashamega <adambashaahmednaji@gmail.com>
2023-11-06 07:23:37 +0200

    Front ui

--- main.js
+++ main.js
@@ -1,24 +1,7 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+import './master.sass'
+const isMobile = window.innerWidth < 768;
+if(isMobile){
+  document.getElementById('divider').src ="public/images/pattern-divider-mobile.svg"
+}else{
+  document.getElementById('divider').src ="public/images/pattern-divider-desktop.svg"
+}
