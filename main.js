f0e946da01ba94cd501110e22f14a7332f468138
Bashamega <adambashaahmednaji@gmail.com>
2023-11-06 10:33:48 +0200

    Finish

--- main.js
+++ main.js
@@ -2,6 +2,19 @@ import './master.sass'
 const isMobile = window.innerWidth < 768;
 if(isMobile){
   document.getElementById('mobile-divider').classList.add('show')
+  document.getElementById('screen').style.maxWidth = "295px"
 }else{
   document.getElementById('desktop-divider').classList.add('show')
 }
+function generate(){
+  fetch('https://api.adviceslip.com/advice')
+  .then(res=>res.json())
+  .then((data) =>{
+    const title = `Advice #${data.slip.id}`
+    const advice = data.slip.advice
+    document.getElementById('title').innerHTML = title
+    document.getElementById('advice').innerHTML = `"${advice}"`
+  })
+}
+generate()
+document.getElementById('random').addEventListener('click', generate)
